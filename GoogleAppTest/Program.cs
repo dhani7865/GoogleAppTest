65aa57b43cfd33ef4ebf2ab523a8d3b4190694c3
dhanyaal <dh11any@hotmail.com>
2022-06-01 14:48:50 +0100

    Google App Test

---
+++ GoogleAppTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace GoogleAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // put all tests here
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.co.uk/");
+            driver.FindElement(By.Id("L2AGLb")).Click();
+            driver.FindElement(By.Name("q")).SendKeys("Learn automation");
+
+        }
+    }
+}
